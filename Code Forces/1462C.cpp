5ce66a1f337f787ab07b9dbba6c60527b57a2895
rakeshbodavula <78262219+rakeshbodavula@users.noreply.github.com>
2021-06-28 19:34:05 +0530

    Update 1462C.cpp

--- Code Forces/1462C.cpp
+++ Code Forces/1462C.cpp
@@ -8,6 +8,53 @@ using namespace std;
 
 void solve(){
     int x;
+    string arr;
+    cin>>x;
+    if(x>45){
+        cout<<-1<<"\n";
+        return;
+    }
+    for(int i=9;i>=1;i--){
+        if(x>=i){
+            arr+=('0'+i);
+            x-=i;
+        }
+        else if(x>0){
+            arr+=('0'+x);
+            x=0;
+        }
+    }
+    reverse(arr.begin(),arr.end());
+    if(x>0) cout<<-1<<"\n";
+    else cout<<arr<<"\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ll t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
+
+//=======================BRUTE FORCE===================
+
+/*
+
+#include<bits/stdc++.h>
+using namespace std;
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define ll long long
+
+
+void solve(){
+    int x;
     cin>>x;
     if(x>45) cout<<-1<<"\n";
     else if(x<10) cout<<x<<"\n";
@@ -34,3 +81,4 @@ int main(){
     while(t--) solve();
     return 0;
 }
+*/
